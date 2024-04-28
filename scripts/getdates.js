792154a8bb28dbac22e5654e5397f49870f99da7
paulomath28 <67445880+paulomath28@users.noreply.github.com>
2024-04-28 00:23:43 -0300

    Add files via upload

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obter o elemento que exibirá o ano atual
+    var currentYearElement = document.getElementById('currentyear');
 
+    // Obter o elemento que exibirá a data de última modificação
+    var lastModifiedElement = document.getElementById('lastModified');
+
+    // Obter o ano atual
+    var currentYear = new Date().getFullYear();
+
+    // Exibir o ano atual no elemento
+    currentYearElement.textContent = currentYear;
+
+    // Obter a data de última modificação do documento
+    var lastModifiedDate = document.lastModified;
+
+    // Exibir a data de última modificação no elemento
+    lastModifiedElement.textContent = "Last modified: " + lastModifiedDate;
+});
