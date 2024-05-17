63dcc0f33cc01861d33d6d3ce50558a17faed299
paulomath28 <67445880+paulomath28@users.noreply.github.com>
2024-05-17 15:22:29 -0300

    Add files via upload

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,158 @@
+// menu navigation button
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation')
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show')
+    hamButton.classList.toggle('show')
+}
+)
+// Array of temple objects
+const temples = [
+    {
+      templeName: "Aba Nigeria",
+      location: "Aba, Nigeria",
+      dedicated: "2005, August, 7",
+      area: 11500,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+      templeName: "Manti Utah",
+      location: "Manti, Utah, United States",
+      dedicated: "1888, May, 21",
+      area: 74792,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+      templeName: "Payson Utah",
+      location: "Payson, Utah, United States",
+      dedicated: "2015, June, 7",
+      area: 96630,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+      templeName: "Yigo Guam",
+      location: "Yigo, Guam",
+      dedicated: "2020, May, 2",
+      area: 6861,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+      templeName: "Washington D.C.",
+      location: "Kensington, Maryland, United States",
+      dedicated: "1974, November, 19",
+      area: 156558,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+      templeName: "Lima Perú",
+      location: "Lima, Perú",
+      dedicated: "1986, January, 10",
+      area: 9600,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+      templeName: "Mexico City Mexico",
+      location: "Mexico City, Mexico",
+      dedicated: "1983, December, 2",
+      area: 116642,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+      templeName: "Columbia River Washington",
+      location: "Columbia River, Washington",
+      dedicated: "2001, November, 18",
+      area: 16880,
+      imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/columbia-river-washington/400x250/columbia-river-washington-temple-1143561-wallpaper.jpg"
+
+    },
+    {
+      templeName: "Copenhagen Denmark",
+      location: "Copenhagen, Denmark",
+      dedicated: "2004, May, 23",
+      area: 25000,
+      imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/copenhagen-denmark/400x250/copenhagen-denmark-temple-lds-278232-wallpaper.jpg"
+
+    },
+    {
+      templeName: "Rome Italy",
+      location: "Rome, Italy",
+      dedicated: "2019, March, 10",
+      area: 41010,
+      imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rome-italy/2019/400x250/2-Rome-Temple-2190090.jpg"
+
+    }
+    
+  ];
+
+function makeCards(templeList) {
+  const container = document.querySelector(".container");
+  templeList.forEach(function (temple)
+  {
+    container.innerHTML += `<div class="temple-card">
+    <h2>${temple.templeName}</h2>
+    <ul>
+      <li><strong><span class="list-title">Location:</span></strong> ${temple.location}</li>
+      <li><strong><span class="list-title">Dedicated:</span></strong> ${temple.dedicated}</li>
+      <li><strong><span class="list-title">Size:</span></strong> ${temple.area}</li>
+    </ul>
+    <figure>
+      <img src="${temple.imageUrl}" alt= "${temple.templeName} Temple" loading="lazy" width="500" height="200">
+    </figure>
+  </div>`
+}
+)};
+
+makeCards(temples);
+
+// make the filter buttons to change what temples are shown depending on the list that's clicked.
+document.querySelector("#home-button").addEventListener('click', function() {
+  document.querySelector("#tab-title").textContent = "Home"
+  let container = document.querySelector(".container")
+  container.innerHTML = '';
+  makeCards(temples);
+})
+document.querySelector("#old-button").addEventListener('click', () => {
+  document.querySelector("#tab-title").textContent = "Old"
+  let container = document.querySelector(".container")
+  container.innerHTML = '';
+  const oldTemples = temples.filter((temple) => parseInt(temple.dedicated.substring(0, 4)) < 1900);
+  makeCards(oldTemples);
+})
+document.querySelector("#new-button").addEventListener('click', () => {
+  document.querySelector("#tab-title").textContent = "New"
+  let container = document.querySelector(".container")
+  container.innerHTML = '';
+  const newTemples = temples.filter((temple) => parseInt(temple.dedicated.substring(0, 4)) > 2000);
+  makeCards(newTemples);
+})
+document.querySelector("#large-button").addEventListener('click', () => {
+  document.querySelector("#tab-title").textContent = "Large"
+  let container = document.querySelector(".container")
+  container.innerHTML = '';
+  const largeTemples = temples.filter((temple) => temple.area > 90000);
+  makeCards(largeTemples);
+})
+document.querySelector("#small-button").addEventListener('click', () => {
+  document.querySelector("#tab-title").textContent = "Small"
+  let container = document.querySelector(".container")
+  container.innerHTML = '';
+  const smallTemples = temples.filter((temple) => temple.area < 10000);
+  makeCards(smallTemples);
+})
+
+// add get dates from index.html to the temples page
+const today = new Date();
+const currentYear = document.querySelector("#currentyear");
+currentYear.innerHTML = today.getFullYear();
+const lastModified = document.querySelector("#lastModified");
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
+
+
