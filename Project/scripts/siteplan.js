db436e3daa4caf90683748ca0014ce993fb421ed
paulomath28 <67445880+paulomath28@users.noreply.github.com>
2024-05-23 23:01:05 -0300

    Add files via upload

--- Project/scripts/siteplan.js
+++ Project/scripts/siteplan.js
@@ -1,1 +1,5 @@
+//get the current year
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 
+//get the date the document was last modified
+document.getElementById("lastmodified").innerHTML = new Date(document.lastModified);
